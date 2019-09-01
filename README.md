7f742382d79d4fef12cc6189f081bdc7607e47fc
Alaa Mohammad <alaa.mohammadhb@gmail.com>
2019-09-01 09:31:31 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Week 11 Assessment
+
+## instructions :
+ 1.  Assessment duration 1 hour ad a half.
+ 2.  You can use only the PHP documentation site.
+ 3. You should fork this repository then upload your answers.
+ 4. All the answers that not following the instructions will be excluded.   
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+/* Question 1 : Write a PHP script to display names and the salaries amounts, within a table. ou should use `echo ` to generate your table. */
+//////////////////////// Your Code Here /////////////////////
+
+
+
+
+
+/*Question 2 : Write a PHP script which displays the capital and country name from the below array $c. as unordered list after sorting the array list by countries names  */
+//////////////////////// Your Code Here /////////////////////
+
+$countries = array( "Italy"=>"Rome",
+              "Luxembourg"=>"Luxembourg",
+               "Belgium"=> "Brussels",
+               "Denmark"=>"Copenhagen",
+               "Finland"=>"Helsinki",
+               "France" => "Paris",
+               "Slovakia"=>"Bratislava" ) ;
+
+/*Question 3 : Write a script to build the following stars pattern, using a nested for loop.
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+
+/*Question 4 : Write a PHP script to calculate the difference between two dates.
+Input : 1981-11-04, 2013-09-04
+Output : 31 years, 10 months, 11 days
+
+
+/*Question 5 : Create a simple HTML form that accept the user name after submitting display the name using PHP echo statement under the form. */
